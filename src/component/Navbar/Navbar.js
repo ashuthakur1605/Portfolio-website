b3fdf5c99132b3c25d3cdaaa15620bb641269285
Ashu <ashu1652002@gmail.com>
2023-04-06 14:10:50 +0530

    HamBurger Attached

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -2,34 +2,38 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css'
 import { useState } from 'react'
-const Navbar=()=> {
-    const [navbar_class, setNavbarclass] = useState("Hnavbar")
-    const [bur_class, setburclass] = useState("burgerl")
+const Navbar = () => {
+    const [Navbar, setNavbar] = useState('naviC')
+    const [Hnavbar_class, setHNavbarclass] = useState("Hnavbar")
+    const [bur_class, setburclass] = useState("burgerl unclicked")
     const [menu_class, setMenuclass] = useState("menu hidden")
-    const [ IsmenuClicked, setIsmenuClicked ] = useState(false)
+    const [IsmenuClicked, setIsmenuClicked] = useState(false)
 
-    const BurClicked = ()=>{
-        if(!IsmenuClicked){
+    const BurClicked = () => {
+        if (!IsmenuClicked) {
             // console.log("visible")
-            setburclass("burgerl")
+            setburclass("burgerl clicked")
             setMenuclass("menu visible")
-            setNavbarclass("Hnavbar")
+            setHNavbarclass("Hnavbar")
+            setNavbar("navi")
 
         }
-        else{
+        else {
             // console.log("hidden")
-            setburclass("burgerl")
+            setburclass("burgerl unclicked")
             setMenuclass("menu hidden")
-            setNavbarclass("Hnavbar")
+            setHNavbarclass("Hnavbar")
+            setNavbar("naviC")
+
         }
         setIsmenuClicked(!IsmenuClicked)
     }
-    
-    
+
+
 
     return (
         <nav id='Navbar'>
-            <div className="navi">
+            <div className={Navbar}>
                 <div className="Logo"></div>
                 <Link to="/Home" id='Home' className='navbar'>Home</Link>
                 <Link to="/About us" id='About us' className='navbar'>About us</Link>
@@ -38,24 +42,24 @@ const Navbar=()=> {
                 <Link to="/Contact us" id='Contact us' className='navbar'>Contact us</Link>
 
                 <Link to="/Login" id='Login' className='contact-us'>Login</Link>
-                
+
                 {/* <Link to="/SignIn" id='Sign-up' className='navbar contact-us'>Sign Up</Link> */}
                 <div className="Hamburger" onClick={BurClicked}  >
                     <div className={bur_class} id='Ham1' ></div>
                     <div className={bur_class} id='Ham2' ></div>
                     <div className={bur_class} id='Ham3' ></div>
                 </div>
-           
+
 
                 <div className={menu_class}>
-                    
+
                     <div className="HamMenu">
 
-                <Link to="/Home" id='Home' className={navbar_class}>Home</Link>
-                <Link to="/About us" id='About us' className={navbar_class}>About us</Link>
-                <Link to="/Contact us" id='Contact us' className={navbar_class}>Contact us</Link>
-                <Link to="/Login" id='Contact us' className={navbar_class}>Login</Link>
-               
+                        <Link to="/Home" id='Home' className={Hnavbar_class}>Home</Link>
+                        <Link to="/About us" id='About us' className={Hnavbar_class}>About us</Link>
+                        <Link to="/Contact us" id='Contact us' className={Hnavbar_class}>Contact us</Link>
+                        <Link to="/Login" id='Contact us' className={Hnavbar_class}>Login</Link>
+
                     </div>
                 </div>
 
